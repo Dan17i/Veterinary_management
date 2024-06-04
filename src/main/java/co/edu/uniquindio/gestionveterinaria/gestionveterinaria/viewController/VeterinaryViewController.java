6ef1087e728b6ec6e8f01eda04f2a5dd454b92c1
jorgeMontoya022 <jorgew.montoyat@uqvirtual.edu.co>
2024-06-04 00:40:10 -0500

    .

--- src/main/java/co/edu/uniquindio/gestionveterinaria/gestionveterinaria/viewController/VeterinaryViewController.java
+++ src/main/java/co/edu/uniquindio/gestionveterinaria/gestionveterinaria/viewController/VeterinaryViewController.java
@@ -1,4 +1,20 @@
 package co.edu.uniquindio.gestionveterinaria.gestionveterinaria.viewController;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+
 public class VeterinaryViewController {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    void initialize() {
+
+    }
+
 }
